fc8fe76b7d51dc68022ec6a16f3451bc22b4b6e4
Andreas Heim <andreas@heim.no>
2023-07-13 13:49:50 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+#
+# Install oh-my-zsh
+
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# install starshio
+
+curl -fsSL https://starship.rs/install.sh | bash
