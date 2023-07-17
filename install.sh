54ae836f7ada1468417a67d699e4efc2e657e861
Andreas Heim <heim@users.noreply.github.com>
2023-07-17 12:09:32 +0000

    change default shell

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 #copy files
 cp aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 cp gitconfig ~/.gitconfig
+
+sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
