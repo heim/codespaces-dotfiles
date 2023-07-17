41fc2ac0c11f417de6da274aa534b15a042c0e7b
Andreas Heim <heim@users.noreply.github.com>
2023-07-17 12:30:28 +0000

    start med readme

--- install.sh
+++ install.sh
@@ -13,4 +13,4 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 cp aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 cp gitconfig ~/.gitconfig
 
-sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+#sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
